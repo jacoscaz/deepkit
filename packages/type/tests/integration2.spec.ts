5a50ff6f94c26fd0344dac23267c3a9431af73a1
Jacopo Scazzosi <jacopo@scazzosi.com>
2026-05-14 20:43:46 +0200

    fix: the unit test for default function expressions was missing a real negative test case

--- packages/type/tests/integration2.spec.ts
+++ packages/type/tests/integration2.spec.ts
@@ -1986,13 +1986,15 @@ test('default function expression', () => {
         id: integer & AutoIncrement & PrimaryKey = 0;
         created: Date = new Date;
         type: string = 'asd';
+        not?: boolean;
     }
 
     const reflection = ReflectionClass.from(post);
     expect(reflection.getProperty('uuid').hasDefaultFunctionExpression()).toBe(true);
-    expect(reflection.getProperty('id').hasDefaultFunctionExpression()).toBe(false);
-    expect(reflection.getProperty('created').hasDefaultFunctionExpression()).toBe(false);
-    expect(reflection.getProperty('type').hasDefaultFunctionExpression()).toBe(false);
+    expect(reflection.getProperty('id').hasDefaultFunctionExpression()).toBe(true);
+    expect(reflection.getProperty('created').hasDefaultFunctionExpression()).toBe(true);
+    expect(reflection.getProperty('type').hasDefaultFunctionExpression()).toBe(true);
+    expect(reflection.getProperty('not').hasDefaultFunctionExpression()).toBe(false);
 
 });
 
